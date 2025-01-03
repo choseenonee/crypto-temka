2945939473b6fd9518c5706e3a10114ce3dfcea8
Zaman Gabdrakhmanov <z.gabdrakhmanov@vkteam.ru>
2025-01-03 10:47:11 +0300

    upd: new cors

--- internal/delivery/middleware/cors.go
+++ internal/delivery/middleware/cors.go
@@ -1,22 +1,35 @@
 package middleware
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func (m Middleware) CORS() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+	//return func(c *gin.Context) {
+	//	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	//	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	//	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	//	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+	//
+	//	if c.Request.Method == "OPTIONS" {
+	//		c.AbortWithStatus(http.StatusNoContent)
+	//		return
+	//	}
+	//
+	//	c.Next()
+	//}
 
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusNoContent)
-			return
-		}
-
-		c.Next()
-	}
+	return cors.New(cors.Config{
+		AllowMethods: []string{"POST", "OPTIONS", "GET", "PUT", "DELETE"},
+		AllowHeaders: []string{"Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization",
+			"accept", "origin", "Cache-Control", "X-Requested-With"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return true
+		},
+		MaxAge: 1 * time.Hour,
+	})
 }
